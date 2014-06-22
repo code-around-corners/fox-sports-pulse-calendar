3cc0a8a8152622f47159a19e539563d08c002de4
Tim Crockford <tim@crockford.id.au>
2014-06-22 15:27:27 +1000

    Added a new GET parameter called 'seed'. This parameter can be randomly populated and is not read anywhere. Hopefully this will force Google Calendar to update the calendar more frequently.

--- pulse.php
+++ pulse.php
@@ -334,7 +334,6 @@
                     $url = str_replace('?t=1&', '?', $url);
                     $shorturl = fspc_yourls_get($url);
                     
-                    
                     $timezone = '';
                     if ( isset($_GET['tz']) ) $timezone = '&tz=' . $_GET['tz'];
                     $gamelength = '';
@@ -347,6 +346,8 @@
                     if ( isset($_GET['sd']) ) $startdate = '&sd=' . $_GET['sd'];
                     $enddate = '';
                     if ( isset($_GET['ed']) ) $enddate = '&ed=' . $_GET['ed'];
+                    $seed = '';
+                    if ( isset($_GET['seed']) ) $seed = '&seed=' . $_GET['seed'];
                     
                     $baseurl = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['SCRIPT_NAME'];
                     $fullurl = $baseurl . '?assoc=' . $assocID . '&club=' . $clubID . '&team=' . $teamID . '&comps=' . $complist;
